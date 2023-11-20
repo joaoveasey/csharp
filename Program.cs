ab6e6a0907f7765c46d514a1f61a64c6bb187c4d
João Veasey Pedro <125090850+joaoveasey@users.noreply.github.com>
2023-11-20 14:35:17 -0300

    curso C# (20/11)

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics;
+
+class Program
+{
+    static void Main()
+    {
+        if(int.TryParse("455.5", out int x))
+        {
+            Console.WriteLine("Success!");
+        }
+        else
+        {
+            Console.WriteLine("Error...");
+        }
+        
+    }
+}
