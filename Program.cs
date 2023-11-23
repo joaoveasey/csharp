91e17db35f0f7757557518327596fd2044e5f301
João Veasey Pedro <125090850+joaoveasey@users.noreply.github.com>
2023-11-23 10:53:13 -0300

    Update Program.cs
    criação de um sistema de conta bancária, com saques e depósitos.

--- Program.cs
+++ Program.cs
@@ -1,17 +1,79 @@
-﻿using System.Diagnostics;
+using System.Diagnostics;
 
 class Program
 {
     static void Main()
     {
-        if(int.TryParse("455.5", out int x))
-        {
-            Console.WriteLine("Success!");
-        }
-        else
-        {
-            Console.WriteLine("Error...");
-        }
+        BankAccount account1 = new BankAccount("Joao", 100);
+        BankAccount account2 = new BankAccount("Tiago", 100);
+
+        account1.Deposit(100);
+        account2.Deposit(100);
+
+        account1.Remove(50);
         
+        Console.WriteLine(account1.GetBalance);
+        Console.WriteLine(account2.GetBalance);
+
+        //Console.WriteLine(account2.GetBalance());
     }
+    
 }
+class BankAccount
+        {
+            private string Name;
+            private decimal Balance;
+
+            public decimal GetBalance
+            {
+                get {return Balance;}
+            }
+
+
+
+            //criação do método construtor
+            public BankAccount(string name, decimal balance) 
+            {
+                //verifica se o valor NAME é nulo ou um espaço em branco
+                if(string.IsNullOrWhiteSpace(name)) //verifica
+                {
+                    throw new ArgumentException ($"Nome inválido", name);
+                }
+                //verifica se o valor BALANCE é menor que zero
+                if(decimal.IsNegative(balance))
+                {
+                    throw new Exception ("O valor NEGATIVO não pode ser inserido");
+                }
+                this.Name = name;
+                this.Balance = balance;
+            }
+
+            public void Deposit(decimal amount)
+            {
+                if(decimal.IsNegative(amount))
+                {
+                    return;     
+                }
+                Balance += amount;
+            }
+
+            public void Remove(decimal withdraw)
+            {
+                if(decimal.IsNegative(withdraw))
+                {
+                    Console.WriteLine("Não há como retirar um valor negativo");
+                    return;     
+                }
+                if(decimal.IsNegative(Balance))
+                {
+                    Console.WriteLine("Seu saldo é negativo");
+                }
+                Balance -= withdraw;
+            }
+
+            //funciona como um get para retornar o valor da conta bancaria
+            // public decimal GetBalance()
+            // {
+            //     return Balance;
+            // }
+        }
